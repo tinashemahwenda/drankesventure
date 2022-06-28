f4f83eacdb974c79e19321592ac7a949384f0577
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2022-06-28 21:50:20 +0200

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class BergApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
